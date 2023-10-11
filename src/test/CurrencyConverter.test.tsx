a798c5950053b0e6372c2ed5674476e989cbbe0c
Liz16k <prohoren4ik@gmail.com>
2023-10-12 02:19:51 +0300

    feat: add tests for currency converter module

---
+++ src/test/CurrencyConverter.test.tsx
@@ -0,0 +1,47 @@
+import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import CurrencyModal from '../components/Quotes/CurrencyModal';
+import { ThemeContext } from '../Contexts';
+import GlobalStyle from '../GlobalStyle';
+import { fakeContextValue, fakeLightTheme } from './mockData';
+
+const renderWithProviders = (component: any, theme?: any, contextValue?: any) => render(
+  <ThemeProvider theme={theme ?? fakeLightTheme}>
+    <ThemeContext.Provider value={contextValue ?? fakeContextValue}>
+      <GlobalStyle />
+      {component}
+    </ThemeContext.Provider>
+  </ThemeProvider>,
+);
+
+jest.mock('../api/currencies', () => ({
+  fetchExchangeRate: jest.fn(async () => Promise.resolve({
+    rate: 1.5,
+    fromCode: 'USD',
+    toCode: 'EUR',
+    lastUpdate: 'today',
+  })),
+}));
+
+describe('Currency converter', () => {
+  test('changes toValue when fromValue changes', async () => {
+    renderWithProviders(<CurrencyModal from="USD" />);
+    const [fromInput, toInput]: any = screen.getAllByPlaceholderText('0');
+
+    await userEvent.type(fromInput, '100');
+    expect(toInput.value).toBe('150');
+  });
+
+  test('changes fromValue when toValue changes', async () => {
+    renderWithProviders(<CurrencyModal from="USD" />);
+    const [fromInput, toInput]: any = screen.getAllByPlaceholderText('0');
+
+    await userEvent.type(toInput, '30');
+    expect(fromInput.value).toBe('20');
+  });
+});
